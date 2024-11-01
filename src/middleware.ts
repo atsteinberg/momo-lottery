e9c296e08f62ab88ac4cf02426fa1f725ed93641
Alex Steinberg <alexander.steinberg@hoval.com>
2024-11-01 10:07:42 +0100

    simplify middleware and protect register page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,44 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-function getBaseUrl() {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-  return 'http://localhost:3000';
-}
-
 export default clerkMiddleware(async (auth, req) => {
-  // Skip onboarding check for API routes and the registration page
+  const { userId } = auth();
+  if (!userId) {
+    if (
+      !req.nextUrl.pathname.startsWith('/api/users') &&
+      req.nextUrl.pathname !== '/unverified'
+    ) {
+      auth().protect();
+    }
+    return NextResponse.next();
+  }
+
   if (
-    req.nextUrl.pathname.startsWith('/api/users') ||
     req.nextUrl.pathname === '/register' ||
-    req.nextUrl.pathname === '/unverified'
+    req.nextUrl.pathname === '/unverified' ||
+    req.nextUrl.pathname.startsWith('/api/users')
   ) {
     return NextResponse.next();
   }
-  const { userId } = auth();
-  if (userId) {
-    const baseUrl = getBaseUrl();
-    const response = await fetch(`${baseUrl}/api/users/${userId}`);
-    if (response.ok) {
-      const user = await response.json();
-      if (!user) {
-        return NextResponse.redirect(new URL('/register', req.url));
-      }
-      if (!user.isVerified) {
-        return NextResponse.redirect(new URL('/unverified', req.url));
-      }
+
+  const baseUrl = `${req.nextUrl.protocol}//${req.nextUrl.host}`;
+  const response = await fetch(`${baseUrl}/api/users/${userId}`);
+  if (response.ok) {
+    const user = await response.json();
+    if (!user) {
+      return NextResponse.redirect(new URL('/register', req.url));
+    }
+    if (!user.isVerified) {
+      return NextResponse.redirect(new URL('/unverified', req.url));
     }
   }
-  auth().protect();
+
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
